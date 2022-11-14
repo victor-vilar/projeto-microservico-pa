7b36c2fed853ee0bc37259cdd3dcaf079a38577d
Victor Vilar <victor.svilar@gmail.com>
2022-11-14 14:29:55 -0300

    adicionando o flyway para fazer os scripts do banco automaticamente.

---
+++ src/main/resources/db/migration/V2__create_table_agenda.sql
@@ -0,0 +1,9 @@
+create table agenda(
+	id serial PRIMARY KEY,
+	descricao varchar(255),
+	data_hora timestamp,
+	data_criacao timestamp,
+	paciente_id integer,
+	CONSTRAINT fk_agenda_paciente FOREIGN KEY(paciente_id) REFERENCES paciente(id)
+
+)
