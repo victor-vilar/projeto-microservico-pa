ba6b6e5a41891d01d959fad3443675c7e6af5c83
Victor Vilar <victor.svilar@gmail.com>
2022-11-13 21:23:26 -0300

    primeiro commit

---
+++ src/test/java/br/com/victorvilar/microservico/MicroservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorvilar.microservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
